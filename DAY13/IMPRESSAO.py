9c93e5b54c8fa5ce6611645e4130905b902dd29c
Humberto Alves Sobrinho <Humberto.Alves.Sobrinho@gmail.com>
2024-09-22 10:24:55 -0300

    IMPRESSÃO

---
+++ DAY13/IMPRESSAO.py
@@ -0,0 +1,5 @@
+word_per_page = 0
+pages = int(input("Digite o numero de páginas.: "))
+word_per_page = int(input("Number of words per page.: "))
+total_words = pages * word_per_page
+print(total_words)
